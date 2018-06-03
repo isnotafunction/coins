e24e65bc9b27fd6252abdd2b9251676587800a18
isnotafunction <ivana.cosgrove@gmail.com>
2018-06-03 09:44:58 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const socketIo = require('socket.io')
+const http = require('http')
+const fetch = require('node-fetch')
+
+const port = process.env.PORT || 5000
+
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+const app = express()
+const server = http.createServer(app)
+
+const io = socketIo(server)
+
+
+server.listen(port, ()=>{
+  console.log(`server is listening on port ${port}`)
+})
