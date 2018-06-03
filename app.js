1af82915ee6ec18888ac0a90787afed63a7e04a8
isnotafunction <ivana.cosgrove@gmail.com>
2018-06-03 10:20:33 +0100

    adds api call and emit

--- app.js
+++ app.js
@@ -13,10 +13,33 @@ app.use(index)
 const server = http.createServer(app)
 const io = socketIo(server)
 
+let interval 
 
+io.on('connection', socket => {
+  console.log('client connected')
+  if(interval){
+    clearInterval(interval)
+  } else {
+    interval = setInterval(()=>{
+      getAPIandEmit()
+    }, 60000)
+  }
+  socket.on("disconnect", () => console.log("Client disconnected"));
+})
 
-
-
+const getAPIandEmit = async socket => {
+ const res = {}
+ try{
+  const data = await fetch(url).then(blob => blob.json()).then(data => data)
+  res.time = data.time.updateduk
+  res.currency = data.chartName
+  res.rate = data.bpi.GBP.rate
+  console.log(res)
+  socket.emit('myData', res)
+ } catch(error){
+   console.error(error.code)
+ }
+}
 
 
 server.listen(port, ()=>{
