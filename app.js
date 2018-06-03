a4cde7481559ac0d05b3de9886cd14ec01c6aa3f
isnotafunction <ivana.cosgrove@gmail.com>
2018-06-03 09:52:19 +0100

    adds index route

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express')
 const socketIo = require('socket.io')
 const http = require('http')
 const fetch = require('node-fetch')
-
 const port = process.env.PORT || 5000
-
 const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
 const app = express()
-const server = http.createServer(app)
 
+const index = require('./routes/index')
+app.use(index)
+
+const server = http.createServer(app)
 const io = socketIo(server)
+
+
+
+
+
 
 
 server.listen(port, ()=>{
