0d95c32d0a934d3d531a301f3fb249bcb0b5c500
isnotafunction <ivana.cosgrove@gmail.com>
2018-06-03 11:49:26 +0100

    adds react component and updates the state with api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIoClient from 'socket.io-client'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(){
+    super()
+    this.state = {
+      response: false,
+      endpoint: 'http://127.0.0.1:5000'
+    }
   }
+
+ componentDidMount(){
+   const {endpoint} = this.state
+   const socket = socketIoClient(endpoint)
+   socket.on('myData', data => {
+     console.log(data)
+    this.setState({response:data})
+   })
+ }
+
+ render(){
+   const {response} = this.state
+   return (
+     <div>
+      {response ? <p> udated: {response.time} - currency: {response.currency} - rate: {response.rate}</p> : <p> Loading ...</p> }
+     </div>
+   )
+  }
+
 }
 
 export default App;
